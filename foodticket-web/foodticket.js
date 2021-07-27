ac5d6c32907cef18e2ccaac916d809d19dcd29fb
Tom Hanekamp <tom.hanekamp@luminis.eu>
2021-07-27 16:44:22 +0200

    Moved the js code to the index.html for now as loading it in a separate file from Github does not work with the Android POC.

--- foodticket-web/foodticket.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    var _selector = document.querySelector('input[name=myCheckbox]');
-    _selector.addEventListener('change', function(event) {
-        window.checkBoxToggle(_selector.checked, function(message) {
-            document.getElementById('value').innerText = message;
-        });
-    });
-}
