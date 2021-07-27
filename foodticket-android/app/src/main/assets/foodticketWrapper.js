b9f619178becff23d80b4653b5a94e49b71295e6
Tom Hanekamp <tom.hanekamp@luminis.eu>
2021-07-27 17:12:20 +0200

    Now expanding on the foodtickerWrapper.

--- foodticket-android/app/src/main/assets/foodticketWrapper.js
+++ foodticket-android/app/src/main/assets/foodticketWrapper.js
@@ -1,3 +1,12 @@
 window.checkBoxToggle = function(toggled, callback) {
-    console.log('testy test');
+	console.log('testy test')
+    window.toggleMessageCallback = callback
+    var message = toggled ? 'Toggle Switch is on' : 'Toggle Switch is off';
+    var executor = window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.toggleMessageHandler
+        ? window.webkit.messageHandlers.toggleMessageHandler : AndroidInterface;
+
+    if (executor != undefined) {
+        // This should be a primitive value (e.g. JSON converted to a string is possible)
+        executor.postMessage(message);
+    }
 }
